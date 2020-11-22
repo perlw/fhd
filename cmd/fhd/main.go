8ae0d9bcc214096be50bf4eed8e1d54b5056172c
Per Lönn Wege <per@perlw.se>
2020-11-22 19:52:29 +0100

    IT WORKS

--- cmd/fhd/main.go
+++ cmd/fhd/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/perlw/fhd/internal/app/fhd/forza"
 )
 
 func main() {
@@ -28,13 +30,26 @@ func main() {
 	go func() {
 		fmt.Printf("listening...\n")
 		buffer := make([]byte, 1500)
+		class := []string{"D", "C", "B", "A", "S1", "S2", "R", "X"}
+		gear := []rune{'R', '1', '2', '3', '4', '5', '6', '7'}
 		for {
-			n, _, err := listener.ReadFromUDP(buffer)
+			_, _, err := listener.ReadFromUDP(buffer)
 			if err != nil {
 				fmt.Printf(err.Error())
 			}
 
-			fmt.Printf("DAT: %X\n\n", buffer[:n])
+			var packet forza.DataPacket
+			packet.FromBytes(buffer)
+			if packet.Running == 0 {
+				continue
+			}
+
+			//fmt.Printf("\rDAT: %X\n", buffer[:n])
+			fmt.Printf(
+				"\rID: %d (%s, PI: %d), Speed: %.2f, RPM: %.2f, Gear: %c",
+				packet.CarID, class[packet.CarClass], packet.CarPerformanceIndex,
+				packet.Speed*3.60, packet.CurrentEngineRpm, gear[packet.Gear],
+			)
 		}
 	}()
 
---
+++ internal/app/fhd/forza/forza.go
@@ -0,0 +1,101 @@
+package forza
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+// DataPacket represents a single packet from the UDP dataport.
+type DataPacket struct {
+	Running                          int32 // 0 in menus, 1 when not.
+	Timestamp                        uint32
+	EngineMaxRpm                     float32
+	EngineIdleRpm                    float32
+	CurrentEngineRpm                 float32
+	AccelerationX                    float32
+	AccelerationY                    float32
+	AccelerationZ                    float32
+	VelocityX                        float32
+	VelocityY                        float32
+	VelocityZ                        float32
+	AngularVelocityX                 float32
+	AngularVelocityY                 float32
+	AngularVelocityZ                 float32
+	Yaw                              float32
+	Pitch                            float32
+	Roll                             float32
+	NormalizedSuspensionFrontLeft    float32 // 0.0 - relaxed, 1.0 - compressed.
+	NormalizedSuspensionFrontRight   float32
+	NormalizedSuspensionRearLeft     float32
+	NormalizedSuspensionReadRight    float32
+	TireSlipRatioFrontLeft           float32 // 0.0 - max grip, 1.0+ - no grip.
+	TireSlipRatioFrontRight          float32
+	TireSlipRatioRearLeft            float32
+	TireSlipRatioRearRight           float32
+	WheelRotationSpeedFrontLeft      float32 // Radians p/sec.
+	WheelRotationSpeedFrontRight     float32
+	WheelRotationSpeedRearLeft       float32
+	WheelRotationSpeedRearRight      float32
+	WheelOnRumbleStripFrontLeft      int32 // 0 when not on rumble strip, 1 when on.
+	WheelOnRumbleStripFrontRight     int32
+	WheelOnRumbleStripRearLeft       int32
+	WheelOnRumbleStripRearRight      int32
+	WheelInPuddleDepthFrontLeft      float32 // 0.0 - no puddle, 1.0 - deep puddle.
+	WheelInPuddleDepthFrontRight     float32
+	WheelInPuddleDepthRearLeft       float32
+	WheelInPuddleDepthRearRight      float32
+	SurfaceRumbleFrontLeft           float32
+	SurfaceRumbleFrontRight          float32
+	SurfaceRumbleRearLeft            float32
+	SurfaceRumbleRearRight           float32
+	TireSlipAngleFrontLeft           float32 // 0.0 - max grip, 1.0+ - no grip.
+	TireSlipAngleFrontRight          float32
+	TireSlipAngleRearLeft            float32
+	TireSlipAngleRearRight           float32
+	TireCombinedSlipFrontLeft        float32 // 0.0 - max grip, 1.0+ - no grip.
+	TireCombinedSlipFrontRight       float32
+	TireCombinedSlipRearLeft         float32
+	TireCombinedSlipRearRight        float32
+	SuspensionTravelMetersFrontLeft  float32
+	SuspensionTravelMetersFrontRight float32
+	SuspensionTravelMetersRearLeft   float32
+	SuspensionTravelMetersRearRight  float32
+	CarID                            int32
+	CarClass                         int32 // 0: D, 1: C, 2: B, 3: A, 4: S1, 5: S2, 6: R, 7: X.
+	CarPerformanceIndex              int32
+	Drivetrain                       int32 // 0: FWD, 1: RWD, 2: AWD.
+	NumCylinders                     int32
+	Unknown                          [12]byte
+	PositionX                        float32
+	PositionY                        float32
+	PositionZ                        float32
+	Speed                            float32 // Meters per second.
+	Power                            float32 // Watts.
+	Toque                            float32 // Newtonmeter.
+	TireTempFrontLeft                float32
+	TireTempFrontRight               float32
+	TireTempRearLeft                 float32
+	TireTempRearRight                float32
+	Boost                            float32
+	Fuel                             float32
+	DistanceTraveled                 float32
+	BestLap                          float32
+	LastLap                          float32
+	CurrentLap                       float32
+	CurrentRaceTime                  float32
+	LapNumber                        uint16
+	RacePosition                     uint8
+	Accel                            uint8
+	Brake                            uint8
+	Clutch                           uint8
+	Handbrake                        uint8
+	Gear                             uint8
+	Steer                            int8
+	NormalizedDrivingLine            int8
+	NormalizedAIBrakeDifference      int8
+}
+
+// FromBytes converts a raw packet into this struct.
+func (p *DataPacket) FromBytes(data []byte) {
+	binary.Read(bytes.NewReader(data), binary.LittleEndian, p)
+}
